17a52418adc2296978106e651516c33047463028
Mgonzalez06 <lisethgonzalez2000@gmail.com>
2022-03-22 09:31:40 -0600

    API Configuration - Liseth

--- index.js
+++ index.js
@@ -1,35 +1,32 @@
-const { Client }  = require('pg');
-const express = require('express');
+const { Client } = require("pg");
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const client = require('./client');
-
+const cors = require("cors");
+const client = require("./client");
 
 //Middlewere
 app.use(cors());
 
 app.use(
-    express.urlencoded({
-        extended: true
-    })
-)
+  express.urlencoded({
+    extended: true,
+  })
+);
 app.use(express.json());
 
-
-
 //import routes
-const testRoute = require('./routes/test');
+const testRoute = require("./routes/test");
 
+app.use("/test", testRoute);
 
-app.use('/test', testRoute);
-
-
-client.connect( err => {
-    if (err) {
-        console.error('connection error', err.stack)
-    } else {
-        console.log('connected to postgresql database. Listen in port localhost:3000')
-    }
+client.connect((err) => {
+  if (err) {
+    console.error("connection error", err.stack);
+  } else {
+    console.log(
+      "connected to postgresql database. Listen in port localhost:3000"
+    );
+  }
 });
 
 // server listing in port 3000
--- routes/test.js
+++ routes/test.js
@@ -1,16 +1,24 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const client = require('../client');
+const client = require("../client");
 
-
-router.get('/', async (req, res) => {
-    try {
-        const response = await client.query(`SELECT NOW();`);
-        res.json(response)
-       
-    } catch(error) {
-        res.json({message: error})
-    }
-})
+router.get("/", async (req, res) => {
+  try {
+    const response = await client.query(`SELECT NOW();`);
+    res.json({
+      code: 6,
+      msg: "",
+      data: response,
+    });
+    res.status(200);
+  } catch (error) {
+    res.status(400);
+    res.json({
+      code: -6,
+      msg: error,
+      data: "",
+    });
+  }
+});
 
 module.exports = router;
