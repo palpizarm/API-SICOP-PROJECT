85e814842d7cec581abb50ae2db12761d6b65240
Fabian Martínez <54187391+fabianmartinez04@users.noreply.github.com>
2022-03-22 21:43:16 -0600

    [update] fix syntax errors

--- routes/categoriesManagement.js
+++ routes/categoriesManagement.js
@@ -5,6 +5,7 @@ const client = require("../client");
 //Insert new category to Category table
 router.post("/", async (req, res) => {
   try {
+
     let date = new Date();
     var month = date.getMonth() + 1;
     var formatDate = date.getFullYear() + "-" + month + "-" + date.getDate();
@@ -14,13 +15,16 @@ router.post("/", async (req, res) => {
       values ( $1, $2, $3, $4);`,
       [req.body.category_id, req.body.name, req.body.user_id, formatDate]
     );
+
     res.status(200);
     res.json({
       code: 6,
       msg: "",
       data: response,
     });
+
   } catch (error) {
+    
     res.status(400);
     res.json({
       code: -6,
--- routes/favoriteInstitutionsManagement.js
+++ routes/favoriteInstitutionsManagement.js
@@ -2,19 +2,24 @@ const express = require("express");
 const router = express.Router();
 const client = require("../client");
 
+
 //Get all favorite institutions that exists in the FavoriteInstitution table
 router.get("/", async (req, res) => {
   try {
+
     const response = await client.query(
       `select * from public."FavoriteInstitution";`
     );
+
     res.status(200);
     res.json({
       code: 6,
       msg: "",
       data: response,
     });
+
   } catch (error) {
+
     res.status(400);
     res.json({
       code: -6,
@@ -24,21 +29,26 @@ router.get("/", async (req, res) => {
   }
 });
 
+
 //Insert new favorite institution to FavoriteInstitution table
 router.post("/", async (req, res) => {
   try {
+
     const response = await client.query(
       `insert into public."FavoriteInstitution" (user_id,institution_id)
           values ($1,$2);`,
       [req.body.user_id, req.body.institution_id]
     );
+
     res.status(200);
     res.json({
       code: 6,
       msg: "",
       data: response,
     });
+
   } catch (error) {
+
     res.status(400);
     res.json({
       code: -6,
@@ -48,20 +58,25 @@ router.post("/", async (req, res) => {
   }
 });
 
+
 //delete by institution ID a specific favorite institution that exist in FavoriteInstitution table
 router.delete("/", async (req, res) => {
+
   try {
     const response = await client.query(
       `delete from public."FavoriteInstitution" where institution_id = $1 ;`,
       [req.body.institution_id]
     );
+
     res.status(200);
     res.json({
       code: 6,
       msg: "",
       data: response,
     });
+
   } catch (error) {
+
     res.status(400);
     res.json({
       code: -6,
@@ -70,4 +85,6 @@ router.delete("/", async (req, res) => {
     });
   }
 });
+
+
 module.exports = router;
