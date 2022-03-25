7cbb4d28626f755e201c5edeb66ccd1cae6b64e1
Mgonzalez06 <lisethgonzalez2000@gmail.com>
2022-03-24 19:18:36 -0600

    [new] get users method

--- routes/gestionCuenta.js
+++ routes/gestionCuenta.js
@@ -1,8 +1,7 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const client = require('../client');
-const { patch } = require('./test');
-
+const client = require("../client");
+const { patch } = require("./test");
 
 /*
 Method: PATCH.
@@ -10,32 +9,93 @@ Description: Inactivate a cliente or maintenance user.
 Request URL: http://localhost:3000/gestionCuenta/inactivateUser
 Request body: {"user_id"}
 */
-router.patch('/inactivateUser', async (req, res) => {
-    try {
 
-        let updateUser = await client.query(`UPDATE public."User"
+router.patch("/inactivateUser", async (req, res) => {
+  try {
+    let updateUser = await client.query(`UPDATE public."User"
                                             SET actived = B'0' 
                                             WHERE user_id = ${req.body.user_id};`);
-       
-        //Successful inactivation
-        res.status(200);
-        res.json({
-            msg: "",
-            data: updateUser,
-            code: 1
-        })  
 
-    } catch(error) {
+    //Successful inactivation
+    res.status(200);
+    res.json({
+      msg: "",
+      data: updateUser,
+      code: 1,
+    });
+  } catch (error) {
+    ////Registration failed
+    res.status(400);
+    res.json({
+      msg: error,
+      data: "",
+      code: -1,
+    });
+  }
+});
 
-        ////Registration failed
-        res.status(400);
-        res.json({
-            msg: error,
-            data: "",
-            code: -1
-        })
-    }
-})
+/*
+Method: GET.
+Description: Get all mantenance users that exists in LicitaTEC.
+Request URL: http://localhost:3000/gestionCuenta/getMaintenanceUsers
+*/
 
+router.get("/getMaintenanceUsers", async (req, res) => {
+  try {
+    let maintenanceUsers = await client.query(
+      `SELECT U.user_id,U.name,U.email FROM public."User" U,public."Role" R 
+      WHERE U.role_id = R.role_id 
+      AND R.name = 'Mantenimiento';`
+    );
+
+    //Successful inactivation
+    res.status(200);
+    res.json({
+      msg: "",
+      data: maintenanceUsers,
+      code: 1,
+    });
+  } catch (error) {
+    ////Registration failed
+    res.status(400);
+    res.json({
+      msg: error,
+      data: "",
+      code: -1,
+    });
+  }
+});
+
+/*
+Method: GET.
+Description: Get all client users that exists in LicitaTEC.
+Request URL: http://localhost:3000/gestionCuenta/getClientUsers
+*/
+
+router.get("/getClientUsers", async (req, res) => {
+  try {
+    let clientUsers = await client.query(
+      `SELECT U.user_id,U.name,U.email FROM public."User" U,public."Role" R 
+        WHERE U.role_id = R.role_id 
+        AND R.name = 'Cliente';`
+    );
+
+    //Successful inactivation
+    res.status(200);
+    res.json({
+      msg: "",
+      data: clientUsers,
+      code: 1,
+    });
+  } catch (error) {
+    ////Registration failed
+    res.status(400);
+    res.json({
+      msg: error,
+      data: "",
+      code: -1,
+    });
+  }
+});
 
 module.exports = router;
